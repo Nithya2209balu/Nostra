45a41c560859822871f6a74b28f43205c728024e
Nithya2209balu <nithyabalu2209@gmail.com>
2024-07-22 12:17:51 +0530

    added js

--- collection.js
+++ collection.js
@@ -20,3 +20,18 @@ search.addEventListener("click",function(){
         }
     }
 })
+
+
+//selectng side navabar,menuicon
+
+var sidenav= document.getElementById("sidenav")
+var menuicon=document.getElementById("menuicon")
+var closenav=document.getElementById("closenav")
+menuicon.addEventListener("click",function()
+{
+   sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
